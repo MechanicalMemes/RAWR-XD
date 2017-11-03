97166f0d471d8b9688e5646f205469cd47de6766
AtecStudios <VictoryForPhil@gmail.com>
2017-11-03 11:16:38 -0700

    Stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RAWRXD_Driver.java
@@ -0,0 +1,165 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="RAWR Drive", group="Iterative Opmode")
+
+public class RAWRXD_Driver extends OpMode
+{
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor rearLeft = null;
+    private DcMotor rearRight = null;
+
+
+
+    private boolean gyroMode = false;
+
+    private double Sensitivity = 1.0;
+    private Controller controller = null;
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        controller = new Controller(gamepad1);
+
+
+        frontLeft  = hardwareMap.get(DcMotor.class, "fl");
+        frontRight = hardwareMap.get(DcMotor.class, "fr");
+
+        rearLeft  = hardwareMap.get(DcMotor.class, "rl");
+        rearRight = hardwareMap.get(DcMotor.class, "rr");
+
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        rearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+    }
+
+
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+        telemetry.addData("Status", "Initialized. Gyro Calibration");
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        controller.Update();
+
+        if(controller.XState == Controller.ButtonState.JUST_PRESSED){
+            gyroMode = !gyroMode;
+        }
+
+
+        final double x = Math.pow(gamepad1.left_stick_x, 3.0);
+        final double y = -Math.pow(gamepad1.left_stick_y, 3.0);
+        final double speed = Math.min(1.0, Math.sqrt(x * x + y * y));
+
+        double direction = Math.atan2(x, y);
+        double robotAngle = direction + Math.PI / 4;
+
+        final double rotation = -Math.pow(gamepad1.right_stick_x, 3.0);
+
+        final double v1 = speed * Math.cos(robotAngle) + rotation;
+        final double v2 = speed * Math.sin(robotAngle) - rotation;
+        final double v3 = speed * Math.sin(robotAngle) + rotation;
+        final double v4 = speed * Math.cos(robotAngle) - rotation;
+
+        frontLeft.setPower(v1 * Sensitivity);
+        frontRight.setPower(v2 * Sensitivity);
+        rearLeft.setPower(v3 * Sensitivity);
+        rearRight.setPower(v4 * Sensitivity);
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("GyroMode", gyroMode);
+        telemetry.addData("Sensitivity",  Sensitivity);
+
+
+        if(controller.DPadUp == Controller.ButtonState.JUST_PRESSED){
+            Sensitivity += 0.25;
+        }
+
+        if(controller.DPadDown == Controller.ButtonState.JUST_PRESSED){
+            Sensitivity -= 0.25;
+        }
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+}
